acee30bbd8f98f6eb215948e38e785a2c20cab4d
Surja11 <stha.surja123@gmail.com>
2025-04-28 18:23:13 +0545

    Added views and templates

--- poll/app/views.py
+++ poll/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from .models import *
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 #Get questions and display them
@@ -14,12 +15,25 @@ def index(request):
 # Show specific questions and choices
 def detail(request, question_id):
   try:
-    question = Question.objects.filter(pk = question_id)
+    question = Question.objects.get(pk = question_id)
   except Question.DoesNotExist:
     raise Http404("Question doesnot exists")
-  return render(request, 'app/result.html',{'question':question})
+  return render(request, 'app/detail.html',{'question':question})
 
 
 def results(request,question_id):
   question = get_object_or_404(Question, pk = question_id)
   return render(request, 'app/result.html',{'question': question})
+
+def vote(request, question_id):
+  #print(request.POST['choice'])
+  question = get_object_or_404(Question, pk = question_id)
+  try:
+    selected_choice = question.choice_set.get(pk = request.POST['choice'])
+  except:
+    return render(request, 'app/detail.html',{'question': question,  'error_message':  "You didn't select a choice",})
+  
+  else: 
+    selected_choice.votes += 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args = (question.id,)))
