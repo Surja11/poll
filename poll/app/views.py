8a47fe57bf7c8d080e5cc66a1dd93cc6a4fdcf7b
Surja11 <stha.surja123@gmail.com>
2025-04-28 13:36:24 +0545

    Added views and templates

--- poll/app/views.py
+++ poll/app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from .models import *
+from django.http import Http404
 # Create your views here.
+
+#Get questions and display them
+def index(request):
+  latest_questions = Question.objects.order_by('-published_date')[:5];
+
+  context = {'latest_questions': latest_questions}
+
+  return render(request, 'app/index.html',context)
+
+# Show specific questions and choices
+def detail(request, question_id):
+  try:
+    question = Question.objects.filter(pk = question_id)
+  except Question.DoesNotExist:
+    raise Http404("Question doesnot exists")
+  return render(request, 'app/result.html',{'question':question})
+
+
+def results(request,question_id):
+  question = get_object_or_404(Question, pk = question_id)
+  return render(request, 'app/result.html',{'question': question})
